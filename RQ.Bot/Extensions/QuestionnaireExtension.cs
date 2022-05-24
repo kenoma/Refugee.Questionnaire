21224152d4b0a0cf3860d15c083482f001681125
kenoma <bghati@yandex.ru>
2022-05-24 22:36:10 +0300

    Close #19

--- RQ.Bot/Extensions/QuestionnaireExtension.cs
+++ RQ.Bot/Extensions/QuestionnaireExtension.cs
@@ -15,7 +15,7 @@ public static class QuestionnaireExtension
 
             if (string.IsNullOrWhiteSpace(pathToQuest))
                 throw new InvalidProgramException("Specify --pathToQuest argument");
-            
+
             var config = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
                 PrepareHeaderForMatch = args => args.Header.ToLower(),
@@ -26,34 +26,15 @@ public static class QuestionnaireExtension
                 DetectDelimiterValues = new[] { ",", ";", "\t" }
             };
             
-            var checkConfig = builder.Configuration["check-config"];
-            
-            try
+            using var reader = new StreamReader(pathToQuest);
+            using var csv = new CsvReader(reader, config);
+            var records = csv.GetRecords<QuestionnaireEntry>()
+                .ToArray();
+
+            return new Questionnaire
             {
-                using var reader = new StreamReader(pathToQuest);
-                using var csv = new CsvReader(reader, config);
-                var records = csv.GetRecords<QuestionnaireEntry>()
-                    .ToArray();
-
-                if (!string.IsNullOrEmpty(checkConfig))
-                {
-                    Environment.Exit(0);
-                }
-
-                return new Questionnaire
-                {
-                    Entries = records
-                };
-            }
-            catch (Exception e)
-            {
-                if (!string.IsNullOrEmpty(checkConfig))
-                {
-                    Environment.Exit(12309);
-                }
-
-                throw;
-            }
+                Entries = records
+            };
         });
 
         return builder;
