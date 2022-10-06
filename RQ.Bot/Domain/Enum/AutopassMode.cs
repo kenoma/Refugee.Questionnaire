86307aeadda296d53e6b5379c8abd6192195ee31
Александр <s207883@yandex.ru>
2022-10-06 23:47:08 +0300

    ref: Исправлено пространство имен. Добавлены комменты.

--- RQ.Bot/Domain/Enum/AutopassMode.cs
+++ RQ.Bot/Domain/Enum/AutopassMode.cs
@@ -1,12 +1,27 @@
-namespace RQ.DTO.Enum;
+namespace RQ.Bot.Domain.Enum;
 
+/// <summary>
+/// Тип сообщения.
+/// </summary>
 public enum AutopassMode
 {
+    /// <summary>
+    /// Не является пропускаемым сообщением.
+    /// </summary>
     None = 0,
 
+    /// <summary>
+    /// Обычное сообщение, которое проскакивает в тексте.
+    /// </summary>
     Simple = 1,
 
+    /// <summary>
+    /// Сообщения, которые всегда идут в начале анкеты.
+    /// </summary>
     Headline = 2,
 
+    /// <summary>
+    /// Сообщения, которые всегда идут в конце.
+    /// </summary>
     Finisher = 3
 }
